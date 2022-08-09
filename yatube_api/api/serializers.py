630033f74dd1a3352a7de06e7f7e32dd8499a0c6
Станислав Крутских <krutskih@inbox.ru>
2022-08-09 16:30:01 +0300

    Привет! Направляю на ревью проект 9го спринта

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,15 +2,22 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Follow, Group, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = ('__all__')
         model = Post
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('__all__')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +26,25 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
+        fields = ('__all__')
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field='username'
+    )
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = ('__all__')
+        model = Follow
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Подписка на самого себя запрещена')
+        return data
